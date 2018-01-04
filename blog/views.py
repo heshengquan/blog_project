ae60055fd41934d577f4c7b68dfbe5c4ce741232
权哥哥 <651980135@qq.com>
2018-01-04 21:53:05 +0800

    PM:blog_second_commit

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from blog.models import Category,Post,Tag
+import markdown
 
 
 # Create your views here.
 # 首页展示
 def index(request):
     post_list=Post.objects.all()
+
+
 
     content={"post_list":post_list}
 
@@ -15,5 +18,19 @@ def index(request):
 # 博客详情页
 def detail(request,id):
     post=get_object_or_404(Post,id=id)
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
+                                  ])
     content={"post":post}
     return render(request,"blog/detail.html",content)
+
+# 归档栏
+def archives(request):
+    post_list=Post.objects.filter(created_time__year='year',created_time__month='month').order_by("-created_time")
+
+    content = {"post_list": post_list}
+
+    return render(request, "blog/index.html", content)
